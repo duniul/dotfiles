4318dc91385e47a4a9e97e1a7d4a69ec1492f1c1
Daniel Grefberg <hello@danielgrefberg.com>
2022-08-10 13:47:38 +0200

    fish: replace awslogin function with awsload

---
+++ .config/fish/custom_functions/awsload.fish
@@ -0,0 +1,49 @@
+function awsload -d "Log in with aws sso and extract credentials with yawsso."
+    set profiles $argv
+
+    # Since there's no way to easily check if we're logged in, try running yawsso immediately.
+    set -l yawsso_result (yawsso -p $profiles)
+
+    # Check if we need to log in first.
+    if [ $status -ne 0 ]
+        if string match -q "*Can not find valid AWS CLI v2 SSO login*" $yawsso_result
+            echo "Not logged in with AWS SSO. Trying to log in first..."
+            echo
+
+            set_color --dim cyan
+            echo "> sso login --profile $profiles[1]"
+            set_color normal
+            set_color --dim
+            set -l sso_login_output (aws sso login --profile $profiles[1] 2>&1 | tee /dev/tty)
+            set_color normal
+            echo
+
+            # aws sso login always exits with 0, so we need to check the output.
+            if string match -q "*error occurred*" $sso_login_output
+                # Failed to log in with AWS SSO.
+                set_color red
+                echo "Failed to log in with AWS SSO."
+                set_color normal
+                exit $status
+            else
+                # Successfully logged in with AWS SSO.
+                # Reload profiles
+                set -l yawsso_result (yawsso -p $profiles)
+            end
+        else
+            # Unknown error happened.
+            set_color red
+            echo "Failed to load AWS SSO profiles!"
+            set_color normal
+
+            echo $yawsso_result
+            exit $status
+        end
+    end
+
+    set -l joined_profiles (string join "\\e[0m, \\e[0;33m" $profiles)
+
+    echo -n "Loaded AWS SSO profiles: "
+    echo -n -e "\\e[0;33m$joined_profiles\\e[0m"
+    echo
+end
