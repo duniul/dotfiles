cbae17a410c71958e772469f18004542fa012175
Daniel Grefberg <hello@danielgrefberg.com>
2026-02-19 12:48:22 +0100

    brewup: remove redundant upgrade call

--- .config/fish/custom_functions/brewup.fish
+++ .config/fish/custom_functions/brewup.fish
@@ -6,7 +6,6 @@ function brewup -d "Update brew, upgrade brew installs, cleanup and run doctor"
     brew -v update
     
     echo "Upgrading Homebrew installs..."
-    brew upgrade
     brew upgrade -v --force-bottle
 
     echo "Cleaning up Homebrew installs..."
