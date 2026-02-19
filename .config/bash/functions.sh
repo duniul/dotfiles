942f15dc2ce1b739e9665080c3b95fe1fb82728a
Daniel Grefberg <hello@danielgrefberg.com>
2026-02-19 11:51:27 +0100

    bash: remove unused "la" function

--- .config/bash/functions.sh
+++ .config/bash/functions.sh
@@ -1,19 +1,5 @@
 # shellcheck shell=bash
 
-# List all files, long format, colorized, permissions in octal
-function la() {
-	# shellcheck disable=SC2012
-	ls -l "$@" | awk '
-    {
-      k=0;
-      for (i=0;i<=8;i++)
-        k+=((substr($1,i+2,1)~/[rwx]/) *2^(8-i));
-      if (k)
-        printf("%0o ",k);
-      printf(" %9s  %3s %2s %5s  %6s  %s %s %s\n", $3, $6, $7, $8, $5, $9,$10, $11);
-    }'
-}
-
 # Print original and gzipped size
 function gz() {
 	printf "%-20s %12s\n" "compression method" "bytes"
