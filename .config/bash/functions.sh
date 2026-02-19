588e42fdcc5c573e004b17305bb121112e890d07
Daniel Grefberg <hello@danielgrefberg.com>
2026-02-19 11:30:15 +0100

    bash: remove unused voltup function

--- .config/bash/functions.sh
+++ .config/bash/functions.sh
@@ -244,20 +244,3 @@ function aws-sso-login() {
 	aws sso login --profile $profile
 	yawsso --profile $profile
 }
-
-# Update all global packages installed with Volta to the latest version
-function voltup() {
-	packagesPath="$VOLTA_HOME/tools/user/packages"
-	voltaFiles=$(ls $packagesPath)
-
-	echo -e "Updating all global packages installed with Volta...\n"
-
-	for file in $voltaFiles; do
-		packagePath="$packagesPath/$file"
-		name=$(jq -r '.name' $packagePath)
-
-		volta install $name
-	done
-
-	echo "Done, all global packages have been updated!"
-}
